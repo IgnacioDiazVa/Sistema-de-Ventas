78f05bc0cd40241c5f655fdb3e86f6252abe0224
simon <simon@172.16.2.164>
2017-12-20 19:51:46 -0300

    Version 1.2 SistemaVenta (Simon)

--- SistemaVentas/src/modelo/ModeloListar.java
+++ SistemaVentas/src/modelo/ModeloListar.java
@@ -5,6 +5,7 @@
  */
 package modelo;
 
+import accesoBD.acceso;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -21,23 +22,55 @@ public class ModeloListar {
     Venta venta;
     ArrayList<DetalleVenta> detalles;
     DetalleVenta detalleVenta;
-    
+    /**
+     * Metodos de Listar de ventas y Detalle de las Ventas
+     * @Simon
+     */
     public ArrayList<Venta> ListarVentas(){
         
         try {
-            String sql="select id_transaccion,fecha,monto_total from transaccion";
+            String sql="select ID_VENTA,FECHA_CONTRATACION,FECHA_TERMINO_OPCIONAL,FECHA_TERMINO_CONTRATO,TIPOVENTA_ID_TIPO,VALOR_VENTA,RUT from venta";
             PreparedStatement preparedStatement =acceso.obtenerInstancia().prepareStatement(sql);
             ResultSet resultSet = preparedStatement.executeQuery(); 
             ventas = new ArrayList();
             while (resultSet.next()){
                 venta = new Venta();
-                //venta.setIdTransaccion(resultSet.getInt("id_transaccion"));
+                venta.setIdVenta(resultSet.getInt("ID_VENTA"));
+//                venta.setFechaContratacion(resultSet.getString("FECHA_CONTRATACION"));
+                venta.setFechaTerminoOpcional(resultSet.getString("FECHA_TERMINO_OPCIONAL"));
+                venta.setFechaTerminoContrato(resultSet.getString("FECHA_TERMINO_CONTRATO"));
+                venta.setTipoVentaId(resultSet.getInt("TIPOVENTA_ID_TIPO"));
+                venta.setValorVenta(resultSet.getInt("VALOR_VENTA"));
+                venta.setRut(resultSet.getString("RUT"));
                 ventas.add(venta);
             }
             return ventas;
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
-            System.out.println(ex.getMessage());                    
+            System.out.println("se cae en el modelo");                    
+            Logger.getLogger(ModeloListar.class.getName()).log(Level.SEVERE, null, ex);
+                return null;
+        }   
+    }
+    public ArrayList<DetalleVenta> ListarDetalleVenta(){
+        
+        try {
+            String sql="select VENTA_ID_VENTA,PRODUCTO_ID_PRODUCTO,PRECIO,CANTIDAD from relation_2v1";
+            PreparedStatement preparedStatement =acceso.obtenerInstancia().prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery(); 
+            detalles = new ArrayList();
+            while (resultSet.next()){
+                detalleVenta = new DetalleVenta();
+                detalleVenta.setVentaIdVenta(resultSet.getInt("VENTA_ID_VENTA"));
+                detalleVenta.setIdProducto(resultSet.getInt("PRODUCTO_ID_PRODUCTO"));
+                detalleVenta.setPrecio(resultSet.getInt("PRECIO"));
+                detalleVenta.setCantidad(resultSet.getInt("CANTIDAD"));
+                detalles.add(detalleVenta);
+            }
+            return detalles;
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            System.out.println("se cae en el modelo");                    
             Logger.getLogger(ModeloListar.class.getName()).log(Level.SEVERE, null, ex);
                 return null;
         }   
