0ac0eace986ab4e2d8b29bcd614564355dc3bd46
Estudio <Estudio@172.16.2.76>
2017-12-20 21:27:41 -0300

    Agregados a las Vistas

--- SistemaVentas/src/vista/Agregar.java
+++ SistemaVentas/src/vista/Agregar.java
@@ -272,6 +272,8 @@ public class Agregar extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
+        Menu menu = new Menu();
+        menu.setVisible(true);
         dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
 
